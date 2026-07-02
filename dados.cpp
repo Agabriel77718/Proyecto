f45c29ef0771b91fb05b2e4c3ea5bd3fdcbd0cbb
Fernan-033 <00008126@uca.edu.sv>
2026-07-02 11:37:21 -0600

    Codigo mejorado

--- dados.cpp
+++ dados.cpp
@@ -1,26 +1,53 @@
 #include <iostream>
 #include <windows.h>
+#include <ctime>
 
 using namespace std;
 
-int main() { 
-    int intentos = 3;
-    int dado1=4, dado2=4;
-   
+void LanzarDados() {
+int dado1 = (rand() % 6) + 1; 
+int dado2 = (rand() % 6) + 1;
+int suma = dado1 + dado2;
 
-    cout << "===Bienvenido al juego de los dados===" << endl;
+cout<<"====✨🎊Resultados🎊✨===="<<endl;
+cout<<"⬜Dado 1:"<<dado1 << "⬜"<<endl;
+cout<<"⬛Dado 2:"<<dado2 << "⬛"<<endl;
+cout<<"⬜Suma:  "<<suma  << "⬜"<<endl;
 
+if (suma >= 8) {
+cout << "🟥🔥🏅Ganaste🏅🤩" << endl;
+cout << "||=================||" << endl;
+} else {
+cout << "🟩😞Perdiste🎭😢" << endl;
+cout << "||==================||" << endl;   
+}
+}
 
- cout<<"Dado1:"<<dado1<<endl;
-cout<<"Dado2:"<<dado2<<endl;
-cout<<"Tu puntuacion final es "<<dado1 + dado2<<endl;
+int main() {
+SetConsoleOutputCP(CP_UTF8);
+srand(time(0));
+char opcion = 0;
+cout<<" === 🎲🎰 Bienvenido 🎰🎲 === "<<endl;
+cout<<"🟥 ========================🍀"<<endl;
+cout<<"⬛=== CASINO DE DADOS ==== 🎲" << endl;
+cout<<"🟥Si la Suma >= 8 = Ganas  ☘️" << endl;
+cout<<"⬛Si la Suma <= 7 = Pierdes🎲" << endl;
+cout<<"🟥=========================🍀" << endl;
 
- if (dado1 + dado2 >= 8) {
- cout <<"Superaste el objetivo! GANASTE" << endl;
- } else {
- cout << ">> No superaste 8." << endl;
- }
+while (opcion != 'E' && opcion != 'e') {
+cout << "Presione 'P' para jugar o 'E' para salir: ";
+cin >> opcion;
 
-
-    return 0;
+if (opcion == 'P' || opcion == 'p') {
+LanzarDados ();
+}
+else if (opcion == 'E' || opcion == 'e') {
+cout<<"Gracias por jugar al Lucky Shot"<<endl;
+}
+else {
+cout << "Opcion no valida. Por favor, presione 'P' para jugar o 'E' para salir." << endl;
+}
+}
+cout << "Que tengas un buen dia, Dios te ama. " << endl;
+return 0;
 }
